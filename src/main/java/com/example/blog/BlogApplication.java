ccce866cf6d464868a9d66523bea9d5667474d9d
ArmanKaykac <35624921+ArmanKaykac@users.noreply.github.com>
2021-02-08 03:00:20 +0300

    Update BlogApplication.java

--- src/main/java/com/example/blog/BlogApplication.java
+++ src/main/java/com/example/blog/BlogApplication.java
@@ -2,9 +2,10 @@ package com.example.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BlogApplication {
+public class BlogApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
